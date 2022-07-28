f2e622abd26f147a70f866d7f65f9145fda6a2ff
Dagore <david.gomez.redondo.dev@gmail.com>
2022-07-28 08:58:34 +0200

    modified for other date format support

--- utils/getDateObj.js
+++ utils/getDateObj.js
@@ -1,6 +1,6 @@
 function getDateObj (param) {
-  const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
-  if (!regex.test(param.trim())) param = Number(param)
+  const regex = /^\d+$/
+  if (regex.test(param.trim())) param = Number(param)
 
   const date = new Date(param)
   const unix = date.getTime()
